d1788975b517b74c0669549f2b98986a88c340bf
zengcanxiang <1372750445@qq.com>
2016-01-08 11:12:20 +0800

    测试，修改

--- Framework/app/src/main/java/com/ane/framework/Base/adapter/BaseRecyclerViewHolder.java
+++ Framework/app/src/main/java/com/ane/framework/Base/adapter/BaseRecyclerViewHolder.java
@@ -1,17 +1,13 @@
 package com.ane.framework.Base.adapter;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseArray;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * Created by zengcanxiang on 2016/1/6.
  */
 public class BaseRecyclerViewHolder extends RecyclerView.ViewHolder {
-
 
     private SparseArray<View> mViews = new SparseArray<View>();
     private View mConvertView;
@@ -22,19 +18,6 @@ public class BaseRecyclerViewHolder extends RecyclerView.ViewHolder {
         mConvertView.setTag(this);
     }
 
-
-    public static BaseRecyclerViewHolder get(Context context,
-                                             View convertView, ViewGroup parent, int layoutId) {
-        if (convertView == null) {
-            View mConvertView = LayoutInflater.from(context).inflate(layoutId, parent,
-                    false);
-            return new BaseRecyclerViewHolder(mConvertView);
-        } else {
-            BaseRecyclerViewHolder bHolder = (BaseRecyclerViewHolder) convertView.getTag();
-            return bHolder;
-        }
-    }
-
     @SuppressWarnings("unchecked")
     public <R extends View> R getView(int viewId) {
         View view = mViews.get(viewId);
@@ -43,9 +26,5 @@ public class BaseRecyclerViewHolder extends RecyclerView.ViewHolder {
             mViews.put(viewId, view);
         }
         return (R) view;
-    }
-
-    public View getConvertView() {
-        return mConvertView;
     }
 }
